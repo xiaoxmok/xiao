3c1e21e468c4b046a930cdfddc9fa1ebb8f8c769
Your Name <you@example.com>
2017-12-05 12:01:15 +0800

    add

--- web_alert_component/js/main.js
+++ web_alert_component/js/main.js
@@ -3,14 +3,28 @@
  */
 require.config({
     paths:{
-        jquery:'jquery-2.2.2.min'
+        jquery:'jquery-2.2.2.min',
+        jqueryUI:'jquery-ui.min'
     }
 });
 
-require(['jquery','window'],function($,w){
+require(['jquery','window','jqueryUI'],function($,w,$UI){
     $('#a').click(function(){
-        new w.Window().alert("welcome!!",function(){
-            console.log("关闭弹窗");
+        new w.Window().alert({
+            content:"welcome!!",
+            title:"这是一个标题",
+            text4AlertBtn:'OK',
+            hasCloseBtn:true,
+            dragHandle:'.window_header',
+            handle4AlertBtn:function(){                         //回调函数
+                console.log("你点击了 "+this.text4AlertBtn+" 按钮，关闭弹窗");
+            },
+            handle4CloseBtn:function(){
+                console.log("你点击了 关闭 按钮，关闭弹窗");
+            },
+            width:300,
+            height:150,
+            y:100
         });
     });
 });
--- web_alert_component/js/window.js
+++ web_alert_component/js/window.js
@@ -3,21 +3,71 @@
  */
 define(['jquery'],function($){
     function Window(){
-
+        this.cfg = {
+            width:'300',
+            height:'200',
+            title:"标题",
+            content: '',
+            handle:null,
+            isDraggable:true,           //是否可拖动
+            dragHandle:'null',          //手动把手
+            hasMask:true,               //是否显示遮罩层
+            skinClassName:null,         //定制皮肤
+            hasCloseBtn:false,          //是否显示关闭按钮
+            text4AlertBtn:'确定',
+            handle4AlertBtn:null,
+            handle4CloseBtn:null
+        }
     };
 
     Window.prototype = {
-        alert:function(content,handle){
-            var boundingBox = $('<div class="window_boundingBox"></div>');
+        alert:function(cfg){
+            var CFG = $.extend(this.cfg,cfg);
+            var boundingBox = $('<div class="window_boundingBox">' +
+                                    '<div class="window_header">'+ CFG.title +'</div>' +
+                                    '<div class="window_content">'+ CFG.content +'</div>' +
+                                    '<div class="window_footer"><input type="button" value="'+ CFG.text4AlertBtn +'" /></div>' +
+                                '</div>'
+                                );
             boundingBox.appendTo("body");
-            boundingBox.html(content);
+            //boundingBox.html(CFG.content);
+            var mask = null;
+            if(CFG.hasMask){
+                mask = $('<div class="window_mask"></div> ');
+                mask.appendTo("body");
+            }
 
-            var btn = $('<input type="button" value="确定">');
-            btn.appendTo(boundingBox);
+            var btn = boundingBox.find('.window_footer input');
+            //btn.appendTo(boundingBox);
             btn.click(function(){
-                handle && handle();
+                CFG.handle4AlertBtn && CFG.handle4AlertBtn();
                 boundingBox.remove();
+                mask.remove();
             });
+            boundingBox.css({
+                width:CFG.width + 'px',
+                height:CFG.height + 'px',
+                left:(CFG.x || (window.innerWidth - CFG.width)/2) + 'px',
+                top:(CFG.y || (window.innerHeight - CFG.height)/2) + 'px'
+            });
+            if(CFG.hasCloseBtn){
+                var closeBtn = $('<div class="window_closeBtn">X</div>');
+                closeBtn.appendTo(boundingBox);
+                closeBtn.click(function(){
+                    CFG.handle4CloseBtn && CFG.handle4CloseBtn();
+                    boundingBox.remove();
+                    mask.remove();
+                });
+            }
+            if(CFG.isDraggable){
+                if(CFG.dragHandle){
+                    boundingBox.draggable({handle:CFG.dragHandle});
+                }else{
+                    boundingBox.draggable();
+                }
+
+            }
+
         },
         confirm:function(){},
         prompt:function(){}
