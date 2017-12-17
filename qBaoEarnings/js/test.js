a2f078cc6a7af00f73d5071627a385466db39380
xiaoxmok <xiaoxm.ok@foxmail.com>
2017-12-17 15:14:31 +0800

    add

--- qBaoEarnings/js/test.js
+++ qBaoEarnings/js/test.js
@@ -6,26 +6,36 @@
 function cal(days, total) {
     //var total = 600674.82;      //总数
     var totals = Number(total);
-    var dailyIncome = 0;            //每日收益
+    var dailyIncome = 0;            //每日签到总收益
 
     var dailyAssistantFee = 6;               //每日订阅助手费用 ,固定6元
 
     var dailyAccountManagementFee = 0;          //每日账户管理费
 
 
-    var allIncome = 0;            //查询时间内的所有收益
-    var allAssistantFee = 6;               //查询时间内的所有订阅助手费用；
-    var allAccountManagementFee = 0;          //查询时间内的所有账户管理费
+    var allIncome = 0;            //查询时间内的每日签到总收益汇总
+    var allAssistantFee = 6;               //查询时间内的订阅助手费用汇总；
+    var allAccountManagementFee = 0;          //查询时间内的账户管理费汇总
+    var monthAccountManagementFee = 0;          //每月的账户管理费汇总
 
     var behavioralInterestRates = 0.0002;        //行为酬劳系数，0.02%
     var fixedBouns = 0;                //固定奖励比例，0.036%
     var fixedBounsPer = 0;                //固定奖励比例，0.036%
     var wagesReward = 0.0006;                //工资奖励比列，0.06%
 
+    var BIR = 0;    //每日行为奖励
+    var FB = 0;     //每日签到工资奖励
+    var WR = 0;     //每日签到固定收益
 
-    var allDaily = [];       //每日收益及手续费
+    var dailyRewardDetails = [];    //每天收益明细
+
+    var allDaily = [];       //每日签到总收益及手续费
     //var allDays = {};       //查询时间内的所有收益及手续费
 
+    //保留两位小数点，不四舍五入
+    function point(num) {
+        return Math.floor(num * 100) / 100
+    }
 
     for (var i = 0; i < days; i++) {
 
@@ -65,16 +75,33 @@ function cal(days, total) {
                 break;
         }
 
-        //每日收益 = 总资产 * （行业酬劳比例+固定奖励比例+工资奖励比例） - 每日助手费用；
-        dailyIncome = Number((totals * (behavioralInterestRates + fixedBouns + wagesReward)).toFixed(2));
+        //数据助手费用是每天的凌晨3点扣除，本程序默认每天签到扣除手续费。
+        totals = totals.subtr(dailyAssistantFee);
 
-        allIncome = Number((allIncome + dailyIncome - dailyAssistantFee).toFixed(2));       //每日的收益汇总，扣除数据助手费用
+        /*if (days / 30) {
+            totals = totals.subtr(monthAccountManagementFee);
+            monthAccountManagementFee = 0;
+        }*/
 
-        totals = Number((totals + dailyIncome - dailyAssistantFee).toFixed(2));     //每日的总资产
+        //每日签到收益 = 总资产 * （行业酬劳比例+固定奖励比例+工资奖励比例）；
+        BIR = point(totals.mul(behavioralInterestRates));
+        FB = point(totals.mul(fixedBouns));
+        WR = point(totals.mul(wagesReward));
+
+        //console.log("BIR:" + BIR + ",FB:" + FB + ",WR" + WR);
+
+        dailyIncome = (BIR.add(FB)).add(WR);
+        //console.log(typeof dailyIncome+"-totals:"+dailyIncome);
+
+        allIncome = (allIncome.add(dailyIncome));       //每日的收益汇总，扣除数据助手费用
+        //console.log(typeof allIncome+"-dailyIncome:"+allIncome);
+
+        totals = (totals.add(dailyIncome));     //每日的总资产
 
         //账户管理费=日余额*0.01/365
-        dailyAccountManagementFee = Number((totals * 0.01 / 365).toFixed(2));
-        allAccountManagementFee = Number((allAccountManagementFee + dailyAccountManagementFee).toFixed(2));
+        dailyAccountManagementFee = point((totals.mul(0.01)).div(365));
+        allAccountManagementFee = point(allAccountManagementFee.add(dailyAccountManagementFee));
+        monthAccountManagementFee = point(monthAccountManagementFee.add(monthAccountManagementFee));
         //console.log(typeof monthAccountManagementFee);
 
 
@@ -86,16 +113,19 @@ function cal(days, total) {
             dailyAssistantFee: dailyAssistantFee
         };
 
+        dailyRewardDetails = [
+            {BIR: BIR, FB: FB, WR: WR}
+        ];
+
         //console.log("第" + (i + 1) + "天，日收益：" + dailyIncome + ",总：" + totals + ",账户管理费：" + dailyAccountManagementFee + ",月管理费：" + allAccountManagementFee + ",数据助手：" + dailyAssistantFee);
     }
     //console.log(all[0].days);
 
     //查询时间内的所有收益及手续费
     var allDays = [
-
         {name: '总资产', value: totals, type: true},
         {name: '总收益', value: allIncome, type: true},
-        {name: '总数据助手费', value: allAssistantFee*days, type: true},
+        {name: '总数据助手费', value: allAssistantFee * days, type: true},
         {name: '总账户管理费', value: allAccountManagementFee, type: true},
         {name: '行为酬劳', value: "0.02%", type: false},
         {name: '工资奖励', value: "0.06%", type: false},
@@ -104,7 +134,8 @@ function cal(days, total) {
 
     return {
         allDaily: allDaily,
-        allDays: allDays
+        allDays: allDays,
+        dailyRewardDetails: dailyRewardDetails
     };
 }
 
