bbe18a1ab3f309703137d6a61be14e4a243ba4a9
xiaoxmok <xiaoxm.ok@foxmail.com>
2018-01-26 19:12:06 +0800

    add

--- nodeDemo/js/inherit.js
+++ nodeDemo/js/inherit.js
@@ -2,40 +2,116 @@
  * Created by xiaoxm on 2017/5/7.
  */
 //js实现继承的方法
+/**
+ * 原型链接继承
+ * @constructor
+ */
 
-
-var object={
-    sleep:function(){
-
-    },
-    fond:function(){
-
-    }
-};
-
-function animal(name){
-    this.name= name || 'animal';
-    var sleep=function(){
-        console.log(this.name+"睡觉");
-    }
+console.log("+++++++++++++++++原型链接继承+++++++++++++++++");
+function Super() {
+    this.propery = true;
+    this.color = ["red","green","3"]
 }
 
-animal.prototype.eat=function(food){
-    console.log(this.name+" 吃 "+food);
+Super.prototype.getSuperValue = function () {
+    return this.propery;
+};
+
+Super.prototype.say = function () {
+    console.log("hello!");
 };
 
 
-//原型链继承
-function Cat(){}
+function Sub() {
+    this.SubPropery = false;
+}
+//继承Super();
+Sub.prototype = new Super();
 
-Cat.prototype = new animal();
-Cat.prototype.name="猫";
+Sub.prototype.getSubValue = function () {
+    return this.SubPropery;
+};
 
-var cat = new Cat();
+//重写父类方法
+Sub.prototype.say = function () {
+    console.log("china!");
+};
 
-//animal.sleep();
-console.log(cat.name);
-console.log(cat.eat('鱼'));
-console.log(cat.sleep());
-//console.log(cat instanceof animal);
-//console.log(cat instanceof Cat);
+
+var instance = new Sub();
+
+console.log(instance.getSubValue());
+console.log(instance.getSuperValue());
+
+instance.color.push("black");
+console.log(instance.color);
+
+var instance2 = new Sub();          //原型链方式，在处理数组时会出现此问题。
+console.log(instance2.color);
+
+
+/**
+ *
+ * @param name
+ * @constructor
+ */
+console.log("+++++++++++++++++借用构造函数的方式+++++++++++++++++");
+function A(name) {
+    this.name = name;
+}
+
+function B() {
+    A.call(this, "张三");
+    this.age = 22;
+}
+
+var B_b = new B();
+
+
+console.log(B_b.name);
+console.log(B_b.age);
+
+
+/**
+ *
+ * @param name
+ * @constructor
+ */
+console.log("+++++++++++++++++组合继承，原型链和构造函数的组合+++++++++++++++++");
+function C(name) {
+    this.name = name;
+    this.color = ["red", "blue", "green"];
+}
+
+C.prototype.say = function () {
+    console.log(this.name);
+};
+
+function D(name, age) {
+    C.call(this, name);
+    this.age = age;
+}
+
+D.prototype = new C();
+D.prototype.constructor = D;
+
+D.prototype.sayAge = function () {
+    console.log(this.age);
+};
+
+
+var D_d = new D("张三", 22);
+
+D_d.color.push("black");
+D_d.sayAge();
+D_d.say();
+console.log(D_d.color);
+
+var D_d2 = new D("王五", 23);
+
+D_d2.sayAge();
+D_d2.say();
+console.log(D_d2.color);    //这种方式，可以解决原型链中 关于数组的问题。
+
+
+
