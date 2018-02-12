712e9fbbbbdd6ed792cb9a579a3e18c316c7c9ce
xiaoxmok <xiaoxm.ok@foxmail.com>
2018-02-13 07:43:19 +0800

    add

---
+++ ES6/test/let-const.js
@@ -0,0 +1,43 @@
+"use strict"
+
+for (let i = 0; i < 10; i++) {
+    //console.log(i);           //只在区域内有效
+}
+
+//console.log(i);
+
+
+var a = [];
+for (let i = 0; i < 10; i++) {
+    a[i] = function () {
+        console.log(i);
+    }
+}
+
+a[4]()
+
+
+//console.log(foo);
+console.log(fun);
+
+var fun = '11';
+//let foo = '22';       //不存在变量提升
+
+// 封闭作用域
+let tmp = 'aaa';
+if (true) {
+    //tmp = 'abc';        // ReferenceError
+    let tmp
+}
+console.log(typeof a);
+if(true){
+    //tmp = 'abc';          //// ReferenceError
+    //console.log(tmp);         // ReferenceError
+    //typeof tmp;         // ReferenceError
+
+    let tmp;
+    console.log(tmp);           // undefined
+
+    tmp = 123;
+    console.log(tmp);           // 123
+}
