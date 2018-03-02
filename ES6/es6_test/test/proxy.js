2802331edef54bef04061539ef1170a029e72c07
xiaoxmok <xiaoxm.ok@foxmail.com>
2018-03-02 17:57:13 +0800

    add

--- ES6/es6_test/test/proxy.js
+++ ES6/es6_test/test/proxy.js
@@ -6,15 +6,40 @@
     }
 
     let monitor = new Proxy(obj, {
+        // 拦截读取属性
         get: function (target, key) {
-            return target[key].replace('2017', '2018');
+            if(target[key]){
+                return target[key].replace('2017', '2018');
+            }
         },
+        // 拦截设置属性
         set: function (target, key, value) {
             if (target.key === 'name') {
                 return target.key = value;
             } else {
                 return target.key;
             }
+        },
+        // 拦截key in object操作
+        has(target, key) {
+            if (key === 'name') {
+                return target[key];
+            } else {
+                return false;
+            }
+        },
+        // 拦截delete
+        deleteProperty(target,key){
+            if(key.indexOf('_')>-1){
+                delete target[key];
+                return true;
+            }else{
+                return target[key];
+            }
+        },
+        // 拦截Object.keys,Object.getOwnPropertySymbol,Object.getOwnPropertyNames
+        ownKeys(target){
+            return Object.keys(target).filter(item=>item!='time');
         }
     });
 
@@ -22,4 +47,24 @@
     monitor.time = '2222';
     monitor.name = 'hello';
     console.log('set', monitor.time, monitor.name);
+    console.log('has', 'name' in monitor,'time' in monitor);
+    // delete monitor.name;
+    // delete monitor._r;
+    console.log('delete', monitor);
+    console.log('ownKey', Object.keys(monitor));
 }
+
+{
+    let obj = {
+        time: '2017-12-20',
+        name: 'net',
+        _r: 123
+    };
+
+    console.log('Reflect get',Reflect.get(obj,'time'));
+    Reflect.set(obj,'name','china');
+    console.log(obj);
+    console.log('Reflect has',Reflect.has(obj,'time'));
+}
+
+
