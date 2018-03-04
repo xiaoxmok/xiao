58e3ef2b56fca297b3533e550e4d8611912f75ab
xiaoxmok <xiaoxm.ok@foxmail.com>
2018-03-04 19:33:45 +0800

    add

--- ES6/es6_demo/tasks/css.js
+++ ES6/es6_demo/tasks/css.js
@@ -1,12 +1,15 @@
 import gulp from 'gulp';
 import gulpif from 'gulp-if';
 import livereload from 'gulp-livereload';
+import concat from 'gulp-concat'
 import args from './util/args';
 import csso from 'gulp-csso';
+import minifyCss from 'gulp-minify-css';        // 压缩处理成一行
 
 gulp.task('css', () => {
     return gulp.src('app/**/*.css')
-        //.pipe(csso())
+        //.pipe(csso())                 // 多个文件时，csso压缩有问题，需要使用minify
+        .pipe(minifyCss())
         .pipe(gulp.dest('server/public'))
         .pipe(gulpif(args.watch,livereload()))
 })
