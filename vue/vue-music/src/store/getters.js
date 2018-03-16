e97fd37661030ad8bea5cba4d84cc4a1c9d6943c
xiaoxmok <xiaoxm.ok@foxmail.com>
2018-03-16 17:39:49 +0800

    add

--- vue/vue-music/src/store/getters.js
+++ vue/vue-music/src/store/getters.js
@@ -1,3 +1,21 @@
 // 对state做一些映射
 
 export const singer = state => state.singer
+
+export const playing = state => state.playing
+
+export const fullScreen = state => state.fullScreen
+
+export const playList = state => state.playList
+
+export const sequenceList = state => state.sequenceList
+
+export const mode = state => state.mode
+
+export const currentIndex = state => state.currentIndex
+
+export const currentSong = (state) => {
+  return stata.playList[state.currentIndex] || {}
+}
+
+
--- vue/vue-music/src/store/state.js
+++ vue/vue-music/src/store/state.js
@@ -1,5 +1,13 @@
+import {playMode} from "common/js/config";
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playList: [],       //播放列表
+  sequenceList: [],   //顺序、随便、单曲
+  mode: playMode.sequence,
+  currentIndex: -1
 }
 
 export default state
