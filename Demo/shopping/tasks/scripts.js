cae36cb39c24e1ea0c63d0e92b4392e183f516ed
xiaoxmok <xiaoxm.ok@foxmail.com>
2018-05-22 18:24:07 +0800

    add

--- Demo/shopping/tasks/scripts.js
+++ Demo/shopping/tasks/scripts.js
@@ -10,8 +10,22 @@ import rename from 'gulp-rename';
 import uglify from 'gulp-uglify';
 import {log, colors} from 'gulp-util';
 import args from './util/args';
+import filter from 'gulp-filter';
 
 gulp.task('scripts', () => {
+    var jsFilter = filter(['!*lib/*'],{restore:true});
+    var condition = function(f){
+        console.log(f.path);
+        console.log(f.path.indexOf('lib'));
+        if(f.path.indexOf('lib') < 0){
+            //console.log(f.path);
+            //console.log(f.path.indexOf('/lib/'));
+            //console.log(f.path.endswith('.min.js'));
+            return false;
+        }
+        return true;
+    };
+
     return gulp.src(['src/**/*.js'])
         .pipe(plumber({
             errorHandler: function () {
@@ -26,6 +40,8 @@ gulp.task('scripts', () => {
             extname: '.min.js'
         }))
         //压缩
+        //.pipe(jsFilter)
+        //.pipe(gulpif(condition,uglify({mangle: true,compress: {properties: true}, output: {'quote_keys': true}})))
         .pipe(uglify({mangle: true,compress: {properties: true}, output: {'quote_keys': true}}))
         //保存文件，存在两个文件，一个压缩，一个未压缩
         .pipe(gulp.dest('dist'))
