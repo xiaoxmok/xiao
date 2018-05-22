8f0a32f858c770107318d564bc289ae50b7a7cf1
xiaoxmok <xiaoxm.ok@foxmail.com>
2018-05-22 14:32:12 +0800

    add

--- Demo/shopping/src/public/js/returns.js
+++ Demo/shopping/src/public/js/returns.js
@@ -1,27 +1,29 @@
-$(function(){
+
+$(function () {
     if (!login()) {
         location.href = "login.html"
     }
 
     //在input file内容改变的时候触发事件
-    $('.file').change(function(){
-        if(URL.createObjectURL($(this)[0].files[0])){
-            $(this).parent().find('.imgshow').attr("src",URL.createObjectURL($(this)[0].files[0]));
+    /*$('.file').change(function () {
+        if (URL.createObjectURL($(this)[0].files[0])) {
+            $(this).parent().find('.imgshow').attr("src", URL.createObjectURL($(this)[0].files[0]));
         }
-    })
+    })*/
 
-    $('#price').keyup(function(){
-        $(this).val($(this).val().replace(/[^\d.]/g,''));
-        if(Number($(this).val()) > 12888){
+    $('#price').keyup(function () {
+        $(this).val($(this).val().replace(/[^\d.]/g, ''));
+        if (Number($(this).val()) > 12888) {
             $('.error').html('金额大了');
-        }else{
+        } else {
             $('.error').html('');
         }
     });
 
+
     var token = getCookie('token');
 
-    $('.submit').click(function(){
+    $('.submit').click(function () {
 
         var goods = $('#goods').children('option:selected').val();
         var type = $('#type').children('option:selected').val();
@@ -30,17 +32,17 @@ $(function(){
         var description = $('#description').val();
 
         var phone = $('#phone').val();
-        if(!CheckMobile(phone)){
+        if (!CheckMobile(phone)) {
             $('.error').html('联系电话格式不正确');
             return;
         }
 
         $.ajax({
-            type:'GET',
-            url:'/'+url+'public/json/returns.json?goods='+goods+'&type='+type+'&reason='+reason+'&price='+price+'&description='+description+'&phone='+phone,
+            type: 'GET',
+            url: '/' + url + 'public/json/returns.json?goods=' + goods + '&type=' + type + '&reason=' + reason + '&price=' + price + '&description=' + description + '&phone=' + phone,
             //url:'http://ustbhuangyi.com/music/api/getDiscList?g_tk=1928093487&inCharset=utf-8&outCharset=utf-8&notice=0&format=json&platform=yqq&hostUin=0&sin=0&ein=29&sortId=5&needNewCode=0&categoryId=10000000&rnd=0.952841328122435',
-            dataType:'json',
-            beforeSend:function(request){
+            dataType: 'json',
+            beforeSend: function (request) {
                 var time = new Date().getTime();
                 var secure_token = $.md5(token + 'shop' + time);
 
@@ -48,26 +50,25 @@ $(function(){
                 request.setRequestHeader("time", time);
                 request.setRequestHeader("secure_token", secure_token);
             },
-            success:function(data){
-                if(data.success === '0'){
-                    if(type==0){
+            success: function (data) {
+                if (data.success === '0') {
+                    if (type == 0) {
                         location.href = "returnsNumber.html"
-                    }else if(type==1){
+                    } else if (type == 1) {
                         location.href = "exchangeGoodsNum.html"
                     }
 
-                }else{
+                } else {
 
                 }
             },
-            error:function(xhr,status,error){
+            error: function (xhr, status, error) {
 
             }
         });
 
 
     });
-
 
 
 })
