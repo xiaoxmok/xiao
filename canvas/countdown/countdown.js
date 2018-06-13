0669d1e39deb1579597a338e2c42d11bebd1a45a
xiaoxmok <xiaoxm.ok@foxmail.com>
2018-06-13 14:49:45 +0800

    add

--- canvas/countdown/countdown.js
+++ canvas/countdown/countdown.js
@@ -5,7 +5,10 @@ var MARGIN_TOP = 60;
 var MARGIN_LEFT = 30;
 var time = null;
 //var endTime = new Date().getTime() + 0.1 * 60 * 1000;
-var endTime = new Date(2018, 5, 12, 19, 20, 6).getTime();
+//var endTime = new Date(2018, 5, 13, 19, 20, 6);   // 设置固定时间
+var endTime = new Date();
+
+endTime.setTime(endTime.getTime() + 1 * 60 * 60 * 1000);        // 设置当前时间+1小时
 
 var curShowTimeSeconds = 0;
 
@@ -13,13 +16,15 @@ var balls = [];
 var colors = ['#33b5e5', '#0099cc', '#aa66cc', '#99CC00', '#669900', '#FFBB33', '#FF8800', '#FF4444', '#CC0000'];
 
 window.onload = function () {
-    //render(context);
+
 
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
 
-    canvas.width = WINDOW_WIDTH;
-    canvas.height = WINDOW_HEIGHT;
+    resize();
+    window.onresize = function () {
+        resize()
+    };
 
     curShowTimeSeconds = getCurrentShowTimeSeconds();
 
@@ -29,6 +34,23 @@ window.onload = function () {
         updata();
     }, 50);
     //render(context);
+};
+
+
+function resize() {
+
+    // 屏幕自适应
+    WINDOW_WIDTH = document.body.clientWidth;
+    WINDOW_HEIGHT = document.body.clientHeight;
+
+    MARGIN_LEFT = Math.round(WINDOW_WIDTH / 10);
+    RADIUS = Math.round(WINDOW_WIDTH * 4 / 5 / 108) - 1;
+    MARGIN_TOP = Math.round(WINDOW_HEIGHT / 5);
+
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
+
+    console.log(WINDOW_WIDTH, WINDOW_HEIGHT);
 }
 
 /**
@@ -73,6 +95,7 @@ function updata() {
     }
 
     updateBalls();
+    //console.log(balls.length);
 
 }
 
@@ -87,9 +110,22 @@ function updateBalls() {
         balls[i].vy += balls[i].g;
         if (balls[i].y >= WINDOW_HEIGHT - RADIUS) {
             balls[i].y = WINDOW_HEIGHT - RADIUS;
-            balls[i].vy = -balls[i].vy * 0.75;
+            balls[i].vy = -balls[i].vy * 0.5;
         }
 
+    }
+
+
+    // 删除出去画布中的小球
+    var cnt = 0;
+    for (var i = 0; i < balls.length; i++) {
+        if (balls[i].x + RADIUS > 0 && balls[i].x - RADIUS < WINDOW_WIDTH) {
+            balls[cnt++] = balls[i];
+        }
+    }
+    // Math.min(300,cnt) 计算cnt到300个小球，当cnt小于300则取cnt，当cnt大于300则取300
+    while (balls.length > Math.min(300, cnt)) {
+        balls.pop();
     }
 }
 
@@ -100,7 +136,7 @@ function updateBalls() {
  * @param num
  */
 function addBalls(x, y, num) {
-    console.log('num',num)
+    //console.log('num', num)
     for (var i = 0; i < digit[num].length; i++) {
         for (var j = 0; j < digit[num][i].length; j++) {
             if (digit[num][i][j] === 1) {
@@ -124,7 +160,11 @@ function addBalls(x, y, num) {
  */
 function getCurrentShowTimeSeconds() {
     var nowTime = new Date();
-    var ret = endTime - nowTime.getTime();
+    // 倒记时效果
+    //var ret = endTime.getTime() - nowTime.getTime();
+
+    // 时钟效果 获取当前时间的 时分秒 的豪秒数
+    var ret = (nowTime.getHours() * 3600 +nowTime.getMinutes() * 60 + nowTime.getSeconds()) * 1000;
 
     return ret >= 0 ? ret : 0;
 }
@@ -168,7 +208,7 @@ function render(cxt) {
         cxt.fillStyle = balls[i].color;
 
         cxt.beginPath();
-        cxt.arc(balls[i].x, balls[i].y, RADIUS, 0, 2 * Math.PI,true);
+        cxt.arc(balls[i].x, balls[i].y, RADIUS, 0, 2 * Math.PI, true);
         cxt.closePath();
 
         cxt.fill();
